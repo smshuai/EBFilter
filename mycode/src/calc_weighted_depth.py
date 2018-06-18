81fa1f3ca701c2ca3d9556b9511c257dc3d0ac0d
sshuai <shimin.shuai@mail.utoronto.ca>
2018-06-18 11:30:25 -0400

    Add an option to disable multi-mapping correction in depth counting

--- mycode/src/calc_weighted_depth.py
+++ mycode/src/calc_weighted_depth.py
@@ -15,21 +15,29 @@ def process_args():
     #
     if len(sys.argv) == 7:
         bam = sys.argv[1]
-        mstat = pd.read_csv(sys.argv[2], index_col='name')
-        umap = np.loadtxt(sys.argv[3], np.str, comments=None)
-        U1U11 = sys.argv[4]
-        donor_id = sys.argv[5]
-        out_path = sys.argv[6]
+        U1U11 = sys.argv[2]
+        donor_id = sys.argv[3]
+        out_path = sys.argv[4]
+        mstat = pd.read_csv(sys.argv[5], index_col='name')
+        umap = np.loadtxt(sys.argv[6], np.str, comments=None)
+        use_weight = 'yes'
+        return bam, mstat, umap, U1U11, donor_id, out_path, use_weight
+    elif len(sys.argv) == 5:
+        bam = sys.argv[1]
+        U1U11 = sys.argv[2]
+        donor_id = sys.argv[3]
+        out_path = sys.argv[4]
+        use_weight = 'no'
+        return bam, None, None, U1U11, donor_id, out_path, use_weight
     else:
-        sys.stderr.write('Usage: calc_weighted_depth.py BAM map_stat umapper U1_bed ID OUT\n')
-        sys.stderr.write('\t- BAM: Bowtie2 re-aligned and post-processed BAM file\n')
-        sys.stderr.write('\t- map_stat: Mapping statistics from post-processing step\n')
-        sys.stderr.write('\t- umapper: Names of unique mapping reads from post-processing step\n')
+        sys.stderr.write('Usage: calc_weighted_depth.py BAM U1_bed ID OUT [map_stat] [umapper]\n')
+        sys.stderr.write('\t- BAM: miniBAM file\n')
         sys.stderr.write('\t- U1_bed: BED file for U1/U11 genes\n')
         sys.stderr.write('\t- ID: sample identifier, also used as output prefix\n')
         sys.stderr.write('\t- OUT: path for output file\n')
+        sys.stderr.write('\t- map_stat: (optional) Mapping statistics from post-processing step\n')
+        sys.stderr.write('\t- umapper: (optional) Names of unique mapping reads from post-processing step\n')
         sys.exit(1)
-    return bam, mstat, umap, U1U11, donor_id, out_path
 
 def get_weights(depth, readBar, ct, umap):
     ''' Use readBar (array of read names), ct (number of times a read maps) and umap (array of umap names)
@@ -148,29 +156,51 @@ def pileup2dict(pileup, mate_order, results, mstat, umap):
                 results[key] = np.add(results.get(key, [0, 0, 0, 0]), counts).tolist()
     return results
 
+def pileup2dict_noweight(pileup, results):
+    with open(pileup) as f:
+        for pp in f:
+            (chrom, pos, _, depth, baseBar, qualBar) = pp.strip().split('\t')
+            depth = int(depth)
+            weights = [1 for i in qualBar]
+            for var in 'ACGT':
+                key = (chrom, pos, var)
+                counts = varWeightedCountCheck(var, depth, baseBar, weights, False)
+                results[key] = np.add(results.get(key, [0, 0, 0, 0]), counts).tolist()
+    return results
+
 if __name__ == '__main__':
     # Get arguments
-    bam, mstat, umap, U1U11, donor_id, out_path = process_args()
-    # Make three pileup files (/1, /2, unpair)
-    cmd1 = 'samtools mpileup -l {} --rf 64 --ff UNMAP,QCFAIL,DUP -a --output-QNAME -Q 15 {} > {}'.format(U1U11, bam, bam + '_1.pileup')
-    cmd2 = 'samtools mpileup -l {} --rf 128 --ff UNMAP,QCFAIL,DUP -a --output-QNAME -Q 15 {} > {}'.format(U1U11, bam, bam + '_2.pileup')
-    cmd3 = 'samtools mpileup -l {} --ff 1,UNMAP,QCFAIL,DUP -a --output-QNAME -Q 15 {} > {}'.format(U1U11, bam, bam + '_3.pileup')
-    run_subprocess(cmd1)
-    run_subprocess(cmd2)
-    run_subprocess(cmd3)
+    bam, mstat, umap, U1U11, donor_id, out_path, use_weight = process_args()
     results = dict()
-    # Process /1 pileup
-    results = pileup2dict(bam + '_1.pileup', '/1', results, mstat, umap)
-    # Process /2 pileup
-    results = pileup2dict(bam + '_2.pileup', '/2', results, mstat, umap)
-    # Process unpair pileup
-    results = pileup2dict(bam + '_3.pileup', '', results, mstat, umap)
+    if use_weight is 'yes':
+        # Make three pileup files (/1, /2, unpair)
+        cmd1 = 'samtools mpileup -l {} -d 1000000 --rf 64 --ff UNMAP,QCFAIL,DUP -a --output-QNAME -Q 15 {} > {}'.format(U1U11, bam, bam + '_1.pileup')
+        cmd2 = 'samtools mpileup -l {} -d 1000000 --rf 128 --ff UNMAP,QCFAIL,DUP -a --output-QNAME -Q 15 {} > {}'.format(U1U11, bam, bam + '_2.pileup')
+        cmd3 = 'samtools mpileup -l {} -d 1000000 --ff 1,UNMAP,QCFAIL,DUP -a --output-QNAME -Q 15 {} > {}'.format(U1U11, bam, bam + '_3.pileup')
+        run_subprocess(cmd1)
+        run_subprocess(cmd2)
+        run_subprocess(cmd3)
+        # Process /1 pileup
+        results = pileup2dict(bam + '_1.pileup', '/1', results, mstat, umap)
+        # Process /2 pileup
+        results = pileup2dict(bam + '_2.pileup', '/2', results, mstat, umap)
+        # Process unpair pileup
+        results = pileup2dict(bam + '_3.pileup', '', results, mstat, umap)
+    else:
+        # No weight
+        cmd = 'samtools mpileup -l {} -d 1000000 --ff UNMAP,QCFAIL,DUP -a -Q 15 {} > {}'.format(U1U11, bam, bam + '.pileup')
+        rub_subprocess(cmd)
+        # Process pileup
+        results = pileup2dict_noweight(bam + '.pileup', results)
     res_DF = pd.DataFrame(results).T.reset_index()
     res_DF.columns = ('chrom', 'pos', 'alt', 'varP', 'dpP', 'varN', 'dpN')
     res_DF['id'] = donor_id
     # Save result
     res_DF.to_csv(out_path, sep='\t', index=False)
     # Clean up
-    os.remove(bam + '_1.pileup')
-    os.remove(bam + '_2.pileup')
-    os.remove(bam + '_3.pileup')
+    if use_weight is 'yes':
+        os.remove(bam + '_1.pileup')
+        os.remove(bam + '_2.pileup')
+        os.remove(bam + '_3.pileup')
+    else:
+        os.remove(bam + '.pileup')
